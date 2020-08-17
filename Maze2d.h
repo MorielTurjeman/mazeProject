01956ab59324b7f4a9c2ff57f29b80169d605eb7
Moriel Turjeman <moriel99@gmail.com>
2020-08-17 10:35:14 +0300

    fix2

--- Maze2d.h
+++ Maze2d.h
@@ -68,38 +68,22 @@ public:
 		std::vector<Position> possibleMoves;
 		int x = p.getXPosition();
 		int y = p.getYPosition();
-<<<<<<< HEAD
-		if ((x < mazeSize-1) && (maze.at(x + 1).at(y)!= 1))
-=======
 		if (((x < mazeSize-1) && (((maze.at(x + 1).at(y)==0) || (maze.at(x + 1).at(y)==2)))))
->>>>>>> eb30feee9a85a9cd5b1e7c8f81dfcb7c5e09f92c
 		{
 			Position p(x + 1, y);
 			possibleMoves.push_back(p);
 		}
-<<<<<<< HEAD
-		if ((x > 0) && (maze.at(x-1).at(y) != 1))
-=======
 		if (((x > 0) && (((maze.at(x-1).at(y)==0)) || (maze.at(x-1).at(y)==2))))
->>>>>>> eb30feee9a85a9cd5b1e7c8f81dfcb7c5e09f92c
 		{
 			Position p(x - 1, y);
 			possibleMoves.push_back(p);
 		}
-<<<<<<< HEAD
-		if ((y < mazeSize-1) && (maze.at(x).at(y+1) != 1))
-=======
 		if (((y < mazeSize-1) && (((maze.at(x).at(y+1)==0) || (maze.at(x).at(y+1)==2)))))
->>>>>>> eb30feee9a85a9cd5b1e7c8f81dfcb7c5e09f92c
 		{
 			Position p(x, y + 1);
 			possibleMoves.push_back(p);
 		}
-<<<<<<< HEAD
-		if ((y > 0) && (maze.at(x).at(y-1)!=1))
-=======
 		if (((y > 0) && (((maze.at(x).at(y-1)==0)) || (maze.at(x).at(y-1)==0))))
->>>>>>> eb30feee9a85a9cd5b1e7c8f81dfcb7c5e09f92c
 		{
 			Position p(x, y - 1);
 			possibleMoves.push_back(p);
