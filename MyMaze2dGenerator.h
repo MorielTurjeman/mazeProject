516cdf839018f25f76642957daff872be154519e
sapire <sapir8886@gmail.com>
2020-08-21 07:49:12 +0300

    dfs maze generator

--- MyMaze2dGenerator.h
+++ MyMaze2dGenerator.h
@@ -24,6 +24,17 @@ public:
         
 
         /*
+        Choose the initial cell, mark it as visited and push it to the stack
+        While the stack is not empty
+            Pop a cell from the stack and make it a current cell
+            If the current cell has any neighbours which have not been visited
+                Push the current cell to the stack
+                Choose one of the unvisited neighbours
+                Remove the wall between the current cell and the chosen cell
+                Mark the chosen cell as visited and push it to the stack
+        */
+
+        /*
         Given a current cell as a parameter,
         Mark the current cell as visited
         While the current cell has any unvisited neighbour cells
