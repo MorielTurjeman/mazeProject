1127b05df11bcb5c9d4895af991039719bf519fe
sapire <sapir8886@gmail.com>
2020-08-23 23:54:08 +0300

    Controller

--- Controller.h
+++ Controller.h
@@ -4,6 +4,9 @@
 
 #ifndef MAZEPROJECT_CONTROLLER_H
 #define MAZEPROJECT_CONTROLLER_H
+#include <unordered_map>
+#include <string>
+#include "Command.h"
 
 
 class Controller
@@ -11,7 +14,17 @@ class Controller
 public:
 	Controller(){}
 	~Controller(){}
+	Command* get(const std::string &command)
+	{
+		auto it = commandMap.find(command);
+		if (it == commandMap.end())
+		{
+			return nullptr;
+		}
+	}
 
+private:
+	std::unordered_map<std::string, Command*> commandMap;
 };
 
 
