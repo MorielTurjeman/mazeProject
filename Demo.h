6d424a3bb4b083305179bfa1361b559fd9b6dca7
sapire <sapir8886@gmail.com>
2020-08-24 14:06:52 +0300

    changes

--- Demo.h
+++ Demo.h
@@ -5,15 +5,30 @@
 #ifndef MAZEPROJECT_DEMO_H
 #define MAZEPROJECT_DEMO_H
 #include "MyMaze2dGenerator.h"
+#include "Searcher.h"
+#include "Searcable.h"
+#include "Maze2dSearchable.h"
 
 
 class Demo
 {
 public:
+	Demo(){}
+	~Demo(){}
 	void run(int size)
 	{
-		Maze2dGeneratorAbs* _maze = new MyMaze2dGenerator;
-		_maze->generate(size);
+		Maze2dGeneratorAbs* _mazeGen = new MyMaze2dGenerator;
+		Maze2d myMaze = _mazeGen->generate(size);
+		myMaze.printMaze();
+
+		Maze2dSearchable s(myMaze);
+		BFS<Position> bfs;
+		auto sol = bfs.search(s);
+		for(auto p : sol.getPath())
+		{
+			std::cout << p->data.getYPosition() << " ," << p->data.getYPosition() << std::endl;
+		}
+		
 
 	}
 };
