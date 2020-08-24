75b8c8637a4d69abaec1c22f7881b3bd6d3b01b4
sapire <sapir8886@gmail.com>
2020-08-24 16:02:26 +0300

    Added 'Observer' and 'Observable'

---
+++ Observable.h
@@ -0,0 +1,16 @@
+//
+// Created by Sapir Ezra on 24/08/2020.
+//
+
+#ifndef MAZEPROJECT_OBSERVABLE_H
+#define MAZEPROJECT_OBSERVABLE_H
+#include "Observer.h"
+
+
+class Observable
+{
+	
+};
+
+
+#endif //MAZEPROJECT_OBSERVABLE_H
---
+++ Observer.h
@@ -0,0 +1,15 @@
+//
+// Created by Sapir Ezra on 24/08/2020.
+//
+
+#ifndef MAZEPROJECT_OBSERVER_H
+#define MAZEPROJECT_OBSERVER_H
+
+
+class Observer
+{
+
+};
+
+
+#endif //MAZEPROJECT_OBSERVER_H
